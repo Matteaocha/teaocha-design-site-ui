ec269219c1bf1a7a4fd38c06734a97c2e6664900
Matthew Bilton <matteaocha@gmail.com>
2021-06-24 14:01:13 +0900

    Jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+const tsconfig = require('./tsconfig.json')
+
 module.exports = {
-  transform: {'^.+\\.ts?$': 'ts-jest'},
+  transform: {'^.+\\.tsx?$': 'ts-jest'},
   testEnvironment: 'jsdom',
   testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "<rootDir>/tests/__mocks__/objectMock.js",
+    "\\.(png|jpg|jpeg|svg|gif|tiff|bmp)$": "<rootDir>/tests/__mocks__/objectMock.js",
+    ...pathsToModuleNameMapper(tsconfig.compilerOptions.paths, { prefix: '<rootDir>/' })
+  },
 }
